2514e2b5302173a4bcbdcf8fd45e4c1b4caac2bc
YutoMatui <157707374+YutoMatui@users.noreply.github.com>
2026-02-22 14:20:05 +0900

    fix

--- client/src/components/v2/FarmersCarouselV2.tsx
+++ client/src/components/v2/FarmersCarouselV2.tsx
@@ -98,7 +98,7 @@ export default function FarmersCarouselV2() {
             x: {
               repeat: Infinity,
               repeatType: "loop",
-              duration: slideDistance > 0 ? slideDistance / 200 : 40,
+              duration: slideDistance > 0 ? slideDistance / 180 : 40,
               ease: "linear",
             },
           }}
